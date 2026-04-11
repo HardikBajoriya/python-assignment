fc7303b5af25aa277be40d79c27fa741a73e47db
HardikBajoriya <bajoriyahardik@gmail.com>
2026-04-11 21:36:27 +0530

    Add files via upload

---
+++ Bank_Class-OBJ.py
@@ -0,0 +1,15 @@
+
+class BankAccount:
+
+    def __init__(self, account_holder, account_number, balance):
+        self.account_holder = account_holder
+        self.account_number = account_number
+        self.balance = balance
+
+    def display_details(self):
+        print("Account Holder:", self.account_holder)
+        print("Account Number:", self.account_number)
+        print("Bank Balance: ₹", self.balance)
+
+account1 = BankAccount("hardik", "694520", 500050)
+account1.display_details()
