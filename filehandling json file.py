b6490a871cf3021ba4dd3f7760da9f74066d0b82
HardikBajoriya <bajoriyahardik@gmail.com>
2026-04-19 22:10:58 +0530

    Add files via upload

---
+++ filehandling json file.py
@@ -0,0 +1,9 @@
+
+
+[
+    {"name":"Hardik","mark":"98","age":19},
+     {"name":"Rahul","mark":"67","age":19},
+      {"name":"Emily","mark":"87","age":18},
+       {"name":"Twen","mark":"56","age":19}
+    
+]
